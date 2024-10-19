a745259f30cf55110d88b68fa6aabf1e6f5bfb23
Fazliradika <49340780+Fazliradika@users.noreply.github.com>
2024-10-19 20:14:26 +0700

    Menambahkan Tugas Besar

---
+++ Tugas Besar.cpp
@@ -0,0 +1,19 @@
+#include <iostream> //library iostream, yang diperlukan untuk input dan output
+using namespace std; //Menggunakan standar namespace
+
+int main() {
+    int a; //Mendeklarasi Variable a menggunakan tipe data integer
+
+    // Meminta input dari pengguna
+    cout << "Masukkan sebuah angka: "; //Menampilkan Masukan sebuah angka untuk diisi pengguna
+    cin >> a; //Membaca input dari pengguna
+
+    // Memeriksa apakah angka tersebut 5 atau bukan
+    if (a == 5) {
+        cout << "Ini adalah angka lima" << endl; // Menampilkan hasil jika Angka Adalah Angka 5
+    } else {
+        cout << "Ini bukan angka lima" << endl;  // Menampilkan hasil jika Angka Bukan Angka 5
+    }
+
+    return 0; //Mengembalikan Nilai 0 sebagai tanda program telah selesai
+}
